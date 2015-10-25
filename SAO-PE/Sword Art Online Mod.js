c2db7c06209aad2332bf2d3cb4ff15660c6c1df5
RedstoneGunMade <redstonegunmade@gmail.com>
2015-10-25 14:27:59 +0000

    Update Sword Art Online Mod.js

--- SAO-PE/Sword Art Online Mod.js
+++ SAO-PE/Sword Art Online Mod.js
@@ -1,13 +1,3 @@
-
-ModPE.setItem(830,"elucidator",0,"Elucidator",1);
-ModPE.setItem(831,"darkrepulser",0,"Dark Repulser",1);
-ModPE.setItem(832,"lambentlight",0,"Lambent Light",1);
-ModPE.setItem(833,"liberator",0,"Liberator",1);
-ModPE.setItem(834,"tyrantdragon",0,"Tyrant Dragon",1);
-ModPE.setItem(835,"objecteraser",0,"Object Eraser",1);
-ModPE.setItem(837,"kagemitsu",0,"Kagemitsu",1);
-ModPE.setItem(838,"excaliber",0,"Excaliber",1);
-
 /*
 ---------------------------
 SAO MCPE Project
